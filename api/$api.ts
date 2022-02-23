cba777233a36c45b53e7cb88cc374610c71d8d7c
shomiyu <m1011522@gmail.com>
2022-02-23 22:45:46 +0900

    aspida追加

---
+++ api/$api.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+// prettier-ignore
+import { AspidaClient, dataToURLString } from 'aspida'
+// prettier-ignore
+import { Methods as Methods0 } from './recipe'
+
+// prettier-ignore
+const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
+  const prefix = (baseURL === undefined ? '' : baseURL).replace(/\/$/, '')
+  const PATH0 = '/recipe'
+  const GET = 'GET'
+
+  return {
+    recipe: {
+      get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
+        fetch<Methods0['get']['resBody']>(prefix, PATH0, GET, option).json(),
+      $get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
+        fetch<Methods0['get']['resBody']>(prefix, PATH0, GET, option).json().then(r => r.body),
+      $path: (option?: { method?: 'get'; query: Methods0['get']['query'] }) =>
+        `${prefix}${PATH0}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`
+    }
+  }
+}
+
+// prettier-ignore
+export type ApiInstance = ReturnType<typeof api>
+// prettier-ignore
+export default api
---
+++ utils/apiClient.ts
@@ -0,0 +1,11 @@
+import api from "../api/$api";
+import aspida from "@aspida/fetch";
+
+const fetchConfig = {
+  headers: {
+    "X-MICROCMS-API-KEY": process.env.API_KEY as string,
+  },
+  baseURL: process.env.BASE_URI as string,
+};
+
+export const apiClient = api(aspida(fetch, fetchConfig));
