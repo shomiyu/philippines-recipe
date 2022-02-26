95edce846184f4e3b1fdcb578cc4d72ab516d127
shomiyu <m1011522@gmail.com>
2022-02-26 17:08:32 +0900

    とりあえず一覧表示

--- src/api/category/index.ts
+++ src/api/category/index.ts
@@ -1,9 +1,9 @@
 import { MicroCMSListResponse, MicroCMSQueries } from "microcms-js-sdk";
-import { Recipe } from "./types";
+import { Category } from "./types";
 
 export type Methods = {
   get: {
     query?: MicroCMSQueries;
-    resBody: MicroCMSListResponse<Recipe>;
+    resBody: MicroCMSListResponse<Category>;
   };
 };
